7ac43d8d8c7b51d7efac5c365c532f0aae738371
KayhanTejani <kayhantejani@hotmail.com>
2022-06-01 16:56:17 +0500

    display live hand landmark annotations on image

--- app.py
+++ app.py
@@ -1,10 +1,32 @@
 import cv2
 import mediapipe as mp
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+
+
 
 cap = cv2.VideoCapture(0)
 
+mp_hands = mp.solutions.hands
+hands = mp_hands.Hands()
+
 while True:
   success, image = cap.read()
+
+  image.flags.writeable = False
+  image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+  results = hands.process(image)
+
+  image.flags.writeable = True
+  image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+  if results.multi_hand_landmarks:
+    for hand_landmarks in results.multi_hand_landmarks:
+      mp_drawing.draw_landmarks(
+            image,
+            hand_landmarks,
+            mp_hands.HAND_CONNECTIONS,
+            mp_drawing_styles.get_default_hand_landmarks_style(),
+            mp_drawing_styles.get_default_hand_connections_style())
 
   cv2.imshow("Image", image)
   cv2.waitKey(1)
